fb9e0cc383221a9d1907a00647cd62a487bb7738
Kirill <45914457+volgariver@users.noreply.github.com>
2023-04-18 23:39:44 +0400

    Add files via upload

---
+++ somrelease.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import streamlit as st
+from minisom import MiniSom
+from pylab import plasma, pcolor, colorbar, plot, show
+
+un  = pd.read_csv('univer_uniq.csv')
+un = un.drop(columns=['Unnamed: 0','federal_district','region_code','okato','id', 'year'])
+
+
+target = un.drop(columns=['federal_district_short','region_name','name','name_short'])
+
+
+label = un.region_name
+target = target.fillna(target.median())
+train = StandardScaler().fit_transform(target.values)
+data = train
+
+
+som_shape = (11, 11)
+som = MiniSom(som_shape[0], som_shape[1], data.shape[1], sigma=1.5, learning_rate=.7, 
+              neighborhood_function='gaussian', random_seed=0)
+
+#som.train_batch(data, 1000, verbose=True)
+som.train(data, 1000, verbose=True)
+
+
+arl = []
+for i, x in enumerate(train):
+    arl.append(str(som.winner(x)[0])  + str(som.winner(x)[1]))
+
+
+dt = pd.DataFrame(arl)
+ans = target
+ans['cluster'] = dt[0]
+ans['region_name'] = un.region_name
+ans['name_short'] = un.name_short
+ans['name'] = un.name
+
+
+st.title('Альтернативные университеты')
+
+option_city = st.selectbox(
+    'Выбирите город целевого ВУЗа',
+    (ans.region_name.unique()))
+    
+    
+option_univer = st.selectbox(
+    'Выбирите ВУЗ',
+    (ans[ans.region_name == option_city].name_short.tolist()))
+    
+fin = ans[ans.region_name == option_city]    
+option_cluster = fin[fin.name_short == option_univer].cluster.tolist()[0]
+outer_df = ans[ans.cluster == option_cluster]
+outer_df = outer_df.drop(outer_df[outer_df.name_short == option_univer].index)
+st.divider()
+st.markdown('**Всего найдено ' + str(outer_df.shape[0]) +' альтернативных вариантов:**')
+
+for i in range(0,outer_df.shape[0]):
+	with st.container():
+		st.subheader(outer_df.name_short.tolist()[i])
+		st.markdown(outer_df.name.tolist()[i])
+		st.markdown(outer_df.region_name.tolist()[i])
+		
+st.divider()	
+	
+agree = st.checkbox('Показать таблицу признаков для выбранного кластера')
+
+if agree:
+    st.dataframe(outer_df)
+   
+   
+image = st.checkbox('Показать матрицу расстояний')
+
+if image:
+	plasma()
+	plt.figure(figsize=(8, 6))
+	pcolor(som.distance_map().T) 
+	colorbar()
+	im = 0
+	for i, x in enumerate(train):
+		w = som.winner(x)
+		plt.text(w[0]+0.2,  w[1]+0.3,  str(ans.cluster.tolist()[i]),color='white', fontdict={'size': 10})
+		im = im + 1
+	st.pyplot(plt)
+
+
