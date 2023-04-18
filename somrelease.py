7b04474023927ed581b9cbe50e8b327047e25467
Kirill <45914457+volgariver@users.noreply.github.com>
2023-04-18 23:49:53 +0400

    Update somrelease.py

--- somrelease.py
+++ somrelease.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
+import sklearn
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import streamlit as st
